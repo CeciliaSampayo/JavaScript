ea75f9a34f8fec6016490a02f627a02f557b8bcc
CeciiNicole <ceciliaanicole@hotmail.com>
2022-08-01 16:46:07 -0300

    ejercicios

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Desafio function
-    
+    /*
     function golesJugador (){
 
     let jugador = prompt("Ingrese el nombre del jugador")
@@ -33,6 +33,27 @@
     
      golesJugador()
  
+
+*/
+
+    let cantidadDinero = parseInt(prompt("Ingrese la cantidad de dinero deseada para gastar y a continuacion vera los produtos que le alcanza para comprar"))
+
+     const JoyeriaBlessed = [
+        {titulo:"Cadena Max", tienda: "JoyeriaBlessed",  precio:2000},
+        {titulo:"Pulsera Dior", tienda: "JoyeriaBlessed", precio:1200},
+        {titulo:"Cadena Chloe", tienda: "JoyeriaBlessed", precio:999},
+        {titulo:"Anillo Sophia", tienda: "JoyeriaBlessed",  precio:1140},
+        {titulo:"Pulsera Danielle", tienda: "JoyeriaBlessed", precio:1650},
+        
+    ]
+
+    
+    for(let productos of JoyeriaBlessed){
+        if(cantidadDinero <= productos.precio){
+            
+            console.log (productos.titulo);
+        }
+    }
 
 
 

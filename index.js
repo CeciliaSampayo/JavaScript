731e68747fa8b77902c8db74147edd0618e7879d
CeciiNicole <ceciliaanicole@hotmail.com>
2022-08-17 20:12:37 -0300

    segunda entrega

---
+++ index.js
@@ -0,0 +1,268 @@
+//FUNCIONES//
+
+function crearProductos() {
+  let ecomerceHtml = document.getElementById("productosHtml");
+  tienda.forEach((producto) => {
+    productosHtml.innerHTML += `<article id="${producto.id}" class="card">
+      <img class="card__img" src="${producto.img}" alt="${producto.nombre}">
+      <h3 class="card__title">${producto.nombre}</h3>
+      <p class="card__info">${producto.descripcion}.</p>
+      <p class="card__price">Precio: $ ${producto.precio}</p>
+      <button class="card__button" id="button_${producto.id}">Agregar al carrito</button>
+  </article>`;
+  });
+}
+
+function muestraCarrito() {
+  let comprasHtml = document.getElementById("comprasHtml");
+  comprasHtml.innerHTML = ``;
+  let totalPrecio = carrito.reduce((acc, el) => acc + el.precio, 0);
+  let carritoHtml = document.getElementById("carritoHtml");
+  if (carrito.length > 0)
+    carritoHtml.innerText = "Su carrito contiene los siguientes productos: ";
+  let nuevaCompra = document.getElementById("comprasHtml");
+  carrito.forEach((productos) => {
+    comprasHtml.innerHTML += `<li>${productos.nombre} $ ${productos.precio}</li><p id=quitar_${productos.id} class= quitar_producto> Quitar producto</p> `;
+  });
+  let totalHtml = document.getElementById("totalHtml");
+  if (carrito.length > 0)
+    totalHtml.innerText = `El total de su compra es de $ ${totalPrecio}`;
+  else totalHtml.innerText = ``;
+}
+
+function compraDeProductos() {
+  let boton_producto1 = document.getElementById("button_1");
+  button_1.onclick = compraProducto1;
+  let boton_producto2 = document.getElementById("button_2");
+  button_2.onclick = compraProducto2;
+  let boton_producto3 = document.getElementById("button_3");
+  button_3.onclick = compraProducto3;
+  let boton_producto4 = document.getElementById("button_4");
+  button_4.onclick = compraProducto4;
+  let boton_producto5 = document.getElementById("button_5");
+  button_5.onclick = compraProducto5;
+}
+
+function quitarProducto() {
+  if (carrito.includes(producto1) == true) {
+    let quitar_producto1 = document.getElementById("quitar_1");
+    quitar_1.onclick = quitarProducto1;
+  }
+  if (carrito.includes(producto2) == true) {
+    let quitar_producto2 = document.getElementById("quitar_2");
+    quitar_2.onclick = quitarProducto2;
+  }
+  if (carrito.includes(producto3) == true) {
+    let quitar_producto3 = document.getElementById("quitar_3");
+    quitar_3.onclick = quitarProducto3;
+  }
+  if (carrito.includes(producto4) == true) {
+    let quitar_producto4 = document.getElementById("quitar_4");
+    quitar_4.onclick = quitarProducto4;
+  }
+  if (carrito.includes(producto5) == true) {
+    let quitar_producto5 = document.getElementById("quitar_5");
+    quitar_5.onclick = quitarProducto5;
+  }
+}
+
+function compraProducto1() {
+  carrito.push(producto1);
+  muestraCarrito();
+  quitarProducto();
+}
+function quitarProducto1() {
+  let producto1Posicion = carrito.indexOf(producto1);
+  carrito.splice(producto1Posicion, 1);
+  muestraCarrito();
+  quitarProducto();
+}
+
+function compraProducto2() {
+  carrito.push(producto2);
+  muestraCarrito();
+  quitarProducto();
+}
+
+function quitarProducto2() {
+  let producto2Posicion = carrito.indexOf(producto2);
+  carrito.splice(producto2Posicion, 1);
+  muestraCarrito();
+  quitarProducto();
+}
+
+function compraProducto3() {
+  carrito.push(producto3);
+  muestraCarrito();
+  quitarProducto();
+}
+
+function quitarProducto3() {
+  let producto3Posicion = carrito.indexOf(producto3);
+  carrito.splice(producto3Posicion, 1);
+  muestraCarrito();
+  quitarProducto();
+}
+
+function compraProducto4() {
+  carrito.push(producto4);
+  muestraCarrito();
+  quitarProducto();
+}
+
+function quitarProducto4() {
+  let producto4Posicion = carrito.indexOf(producto4);
+  carrito.splice(producto4Posicion, 1);
+  muestraCarrito();
+  quitarProducto();
+}
+
+function compraProducto5() {
+  carrito.push(producto5);
+  muestraCarrito();
+  quitarProducto();
+}
+
+function quitarProducto5() {
+  let producto5Posicion = carrito.indexOf(producto5);
+  carrito.splice(producto5Posicion, 1);
+  console.log(carrito);
+  muestraCarrito();
+  quitarProducto();
+}
+
+function finalizarCompra() {
+  let totalPrecio = carrito.reduce((acc, el) => acc + el.precio, 0);
+  const compras = carrito.map((el) => el.nombre);
+
+  alert(
+    "Usted compro: "  +
+    compras +
+    " por el precio final de $ "  +
+    totalPrecio +
+    ".   ¡Muchas gracias! Vuelva pronto."
+  );
+}
+
+function ecommerce() {
+  crearProductos();
+  compraDeProductos();
+
+  let finalizar = document.getElementById("finalizar");
+  finalizar.onclick = finalizarCompra;
+}
+
+//Constructora//
+
+class Productos {
+  constructor(id, img, nombre, descripcion, precio) {
+    (this.id = id),
+      (this.img = img),
+      (this.nombre = nombre.toLocaleUpperCase()),
+      (this.descripcion = descripcion),
+      (this.precio = precio);
+  }
+}
+
+//Productos//
+
+const producto1 = new Productos(
+  1,
+  "assets/7.jpg",
+  "DIAMOND CHAIN IN ROSE GOLD",
+  "Montado en oro rosa de 18k con 300 diamantes blancos",
+  574.201
+);
+
+const producto2 = new Productos(
+  2,
+  "assets/3.webp",
+  "SPIKE DIAMOND BRACELET",
+  " Montado en oro rosado de 18k con 22 diamantes blancos",
+  240.155
+);
+
+const producto3 = new Productos(
+  3,
+  "assets/2.jpg",
+  "NECKLACE IN ROSE GOLD",
+  "Set en oro rosa de 18k con 60 diamantes blancos",
+  340.145
+);
+
+const producto4 = new Productos(
+  4,
+  "assets/5.jpg",
+  "DIAMOND SPIKE ONE BRACELET",
+  "Montado en oro rosa y oro blanco de 18k con 7 diamantes blancos",
+  155.248
+);
+
+const producto5 = new Productos(
+  5,
+  "assets/1.jpg",
+  "NECKLACE IN WHITE GOLD FULL",
+  "Necklace montado en oro blanco de 18K con 580 diamantes blancos",
+  965.451
+);
+
+//Arrays//
+
+const tienda = [producto1, producto2, producto3, producto4, producto5];
+console.log(tienda);
+
+const carrito = [];
+
+ecommerce();
+
+//Storage
+
+let btnDarkMode = document.getElementById("botonDarkMode");
+let btnLighthMode = document.getElementById("botonLightMode");
+
+
+let darkMode 
+if (localStorage.getItem("darkMode")){
+darkMode = localStorage.getItem("darkMode")
+}else{
+  localStorage.setItem("darkMode", "light")
+}
+if(darkMode == "dark"){
+  document.body.classList.add("darkMode")
+}
+btnDarkMode.addEventListener("click", () => {
+  document.body.classList.add("darkMode")
+  localStorage.setItem("darkMode", "dark")
+})
+btnLighthMode.addEventListener("click", () => {
+  document.body.classList.remove("darkMode")
+  localStorage.setItem("darkMode", "light")
+
+})
+
+//storage array tienda
+
+if(localStorage.getItem("tienda")){
+  let tienda = JSON.parse(localStorage.getItem("tienda"))
+  
+}else{
+  console.log(`primera vez que carga Tienda`)
+  tienda.push(producto1,producto2,producto3,producto4,producto5)
+  localStorage.setItem("tienda",JSON.stringify(tienda))
+}
+console.log(tienda)
+
+//storage array carrito
+
+if(localStorage.getItem("carrito")){
+  Productos = JSON.parse(localStorage.getItem("carrito"))
+}else{
+  console.log(`Primera vez que carga Carrito`)
+  localStorage.setItem("carrito", [])
+  
+}
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 class Usuarios {
-  constructor(id,NombreYApellido, documento, sexo, cantidadDeAños) {
-    this.id= id,
-    this.NombreYApellido = NombreYApellido,
+  constructor(id, NombreYApellido, documento, sexo, cantidadDeAños) {
+    this.id = id,
+      this.NombreYApellido = NombreYApellido,
       this.documento = documento,
       this.sexo = sexo,
       this.cantidadDeAños = cantidadDeAños
@@ -17,51 +17,38 @@ let datosUsuarios = []
 const usuarios1 = new Usuarios(1, "Rocio Carrizo", 41547874, "mujer", 23)
 datosUsuarios.push(usuarios1)
 
-const usuarios2 = new Usuarios(2,"Marcela Cattorini", 22000531, "mujer", 54)
+const usuarios2 = new Usuarios(2, "Marcela Cattorini", 22000531, "mujer", 54)
 datosUsuarios.push(usuarios2)
 
 const usuarios3 = new Usuarios(3, "Norberto Angel", 51547874, "hombre", 73)
 datosUsuarios.push(usuarios3)
-/*
 
-function nuevoUsuario() {
-  let nombreCompleto = prompt("Por favor, ingrese su Nombre y Apellido");
-  let documento = parseFloat(prompt("Por favor ingrese su numero de documento"));
-  let sexo = prompt("Por favor ingrese su sexo");
-  let cantidadDeAños = parseFloat(prompt("Por favor ingrese su edad"));
-  let nuevoUsuario = new Usuarios(datosUsuarios.length + 1, nombreCompleto, documento, sexo, cantidadDeAños);
+
+function guardarDatos() {
+  let nombreInput = document.getElementById("nombreInput")
+  let documentoInput = document.getElementById("documentoInput")
+  let sexoInput = document.getElementById("sexoInput")
+  let aniosInput = document.getElementById("aniosInput")
+  let nuevoUsuario = new Usuarios(datosUsuarios.length + 1, nombreInput.value, documentoInput.value, sexoInput.value, aniosInput.value)
+  console.log(nuevoUsuario)
   datosUsuarios.push(nuevoUsuario)
-
 }
-nuevoUsuario()
-*/
 
-
-function guardarDatos(){
-let nombreInput = document.getElementById("nombreInput")
-let documentoInput = document.getElementById("documentoInput")
-let sexoInput = document.getElementById("sexoInput")
-let aniosInput = document.getElementById("aniosInput")
-let nuevoUsuario = new Usuarios (datosUsuarios.length + 1,nombreInput.value, documentoInput.value,sexoInput.value,aniosInput.value)
-console.log(nuevoUsuario)
-datosUsuarios.push(nuevoUsuario)
-}
 const guardarDatosBtn = document.getElementById("guardarUsuarioBtn")
-guardarDatosBtn.addEventListener("click",guardarDatos)
+guardarDatosBtn.addEventListener("click", guardarDatos)
 
 datosUsuarios.forEach((usuarios) => console.log(usuarios))
-
 
 
 //PLANTILLA, DOM
 
 let divUsuarios = document.getElementsByClassName("usuarioss")[0]
 
-function verUsuarios() { 
-  divUsuarios.innerHTML ="";
-datosUsuarios.forEach((usuario)=>{
-  let nuevo = document.createElement("div")
-  nuevo.innerHTML =  `<article id="${usuario.id}" class="card">
+function verUsuarios() {
+  divUsuarios.innerHTML = "";
+  datosUsuarios.forEach((usuario) => {
+    let nuevo = document.createElement("div")
+    nuevo.innerHTML = `<article id="${usuario.id}" class="card">
                                   <hr>
                                   <div class="contentCard">
                                       <p class="nombreApellido">
@@ -76,278 +63,43 @@ datosUsuarios.forEach((usuario)=>{
                                       <p class="añoDeNacimiento">
                                           Año de nacimiento: <br> ${usuario.cantidadDeAños}
                                       </p>
-                                      <a class="btnIngresar" href="">Ver mas informacion</a>
+                                     <button id="agregarBtn${usuario.id}">Ver mas informacion</button>
                                   </div>
                                   </article>`
-  
-  divUsuarios.appendChild(nuevo)
-})
+
+    divUsuarios.appendChild(nuevo)
+
+    //boton agregar 
+
+    let btnAgregarUsuario = document.getElementById(`agregarBtn${usuario.id}`)
+    console.log(btnAgregarUsuario);
+
+   //invocar AgregarUsuarios
+    btnAgregarUsuario.addEventListener("click", () =>{agregarUsuarios(usuario)})
+
+  }) 
+
 }
+
+ function agregarUsuarios(usuario) {
+      console.log(`El usuario ${usuario.NombreYApellido} ha sido registrado con exito. Nº de indetificacion es ${usuario.id}`)
+    }
+
 
 let mostrarUsuariosBtn = document.getElementById("verUsuarios")
 mostrarUsuariosBtn.addEventListener("click", verUsuarios)
 
+// STORAGE
 
-
-
-
-/*
-class Usuarios {
-  constructor(id,NombreYApellido, documento, sexo, cantidadDeAños) {
-    this.id= id,
-    this.NombreYApellido = NombreYApellido,
-      this.documento = documento,
-      this.sexo = sexo,
-      this.cantidadDeAños = cantidadDeAños
-    this.bienvenido = function () {
-      console.log(`${this.NombreYApellido} bienvenido a Blessed Joyas. Su usuario se ha creado exitosamente!`)
-    }
-  }
+if (localStorage.getItem("datosUsuarios")) {
+  datosUsuarios = JSON.parse(localStorage.getItem("datosUsuarios"))
+  console.log(datosUsuarios)
+} else {
+  console.log(`primera vez que carga DatosUsuarios`)
+  datosUsuarios.push(usuarios1, usuarios2, usuarios3)
+  localStorage.setItem("datosUsuarios", JSON.stringify(datosUsuarios))
 }
 
-let datosUsuarios = []
-
-const usuarios1 = new Usuarios(1, "Rocio Carrizo", 41547874, "mujer", 23)
-datosUsuarios.push(usuarios1)
-
-const usuarios2 = new Usuarios(2,"Marcela Cattorini", 22000531, "mujer", 54)
-datosUsuarios.push(usuarios2)
-
-const usuarios3 = new Usuarios(3, "Norberto Angel", 51547874, "hombre", 73)
-datosUsuarios.push(usuarios3)
 
 
-function nuevoUsuario() {
-  let nombreCompleto = prompt("Por favor, ingrese su Nombre y Apellido");
-  let documento = parseFloat(prompt("Por favor ingrese su numero de documento"));
-  let sexo = prompt("Por favor ingrese su sexo");
-  let cantidadDeAños = parseFloat(prompt("Por favor ingrese su edad"));
-  let nuevoUsuario = new Usuarios(datosUsuarios.length + 1, nombreCompleto, documento, sexo, cantidadDeAños);
-  datosUsuarios.push(nuevoUsuario)
 
-}
-nuevoUsuario()
-usuarios3.bienvenido()
-
-datosUsuarios.forEach((usuarios) => console.log(usuarios))
-
-const JOYAS = [
-  {
-    nombre: "DIAMOND CHAIN IN ROSE GOLD",
-    precio: 100000,
-    material: {
-      material1: "Oro blanco",
-      material2: "Oro rosado",
-    },
-  },
-  {
-    nombre: "NECKLACE IN ROSE GOLD",
-    precio: 122000,
-    material: {
-      material1: "Oro blanco",
-      material2: "Oro rosado",
-    },
-  },
-  {
-    nombre: "DIAMOND CHAIN IN ROSE WHITE",
-    precio: 164400,
-    material: {
-      material1: "Oro blanco",
-      material2: "Oro rosado",
-    },
-  },
-  {
-    nombre: "NECKLACE SPIKE DIAMOND",
-    precio: 140300,
-    material: {
-      material1: "Oro blanco",
-      material2: "Oro rosado",
-    },
-  },
-  {
-    nombre: "SPIKE DIAMOND BRACELET",
-    precio: 110200,
-    material: {
-      material1: "Oro blanco",
-      material2: "Oro rosado",
-    },
-  },
-
-];
-
-const JoyeriaBlessed = [
-  { titulo: "DIAMOND CHAIN IN ROSE GOLD", tienda: "JoyeriaBlessed", precio: 100000 },
-  { titulo: "NECKLACE IN ROSE GOLD", tienda: "JoyeriaBlessed", precio: 122000 },
-  { titulo: "DIAMOND CHAIN IN ROSE WHITE", tienda: "JoyeriaBlessed", precio: 164400 },
-  { titulo: "NECKLACE SPIKE DIAMOND", tienda: "JoyeriaBlessed", precio: 140300 },
-  { titulo: "SPIKE DIAMOND BRACELET", tienda: "JoyeriaBlessed", precio: 110200 },
-
-]
-
-const COMPRASCARRITO = [];
-
-function eleccionJoyas() {
-  let joyas =
-    prompt(`Gracias por registrarte! Elija el numero que segun corresponda para comprar nuestras joyas!
-    1. Nombre: ${JOYAS[0].nombre},
-    Precio: ${JOYAS[1].precio} 
-    2. Nombre: ${JOYAS[1].nombre},
-    Precio: ${JOYAS[1].precio}
-    3. Nombre: ${JOYAS[2].nombre},
-    Precio: ${JOYAS[2].precio}
-    4. Nombre: ${JOYAS[3].nombre},
-    Precio: ${JOYAS[3].precio} 
-    5. Nombre: ${JOYAS[4].nombre},
-    Precio: ${JOYAS[4].precio}   `);
-
-  eleccionMaterial(joyas);
-
-}
-
-eleccionJoyas();
-
-function eleccionMaterial(joyas) {
-  switch (joyas) {
-    case "1":
-      tipoDeMaterial("1");
-      break;
-    case "2":
-      tipoDeMaterial("2");
-      break;
-    case "3":
-      tipoDeMaterial("3");
-      break;
-    case "4":
-      tipoDeMaterial("4");
-      break;
-    case "5":
-      tipoDeMateriale("5");
-      break;
-
-    default:
-      alert("Por favor ingresa un numero valido");
-      break;
-  }
-}
-
-function tipoDeMaterial(material) {
-  let oro = "";
-
-  switch (material) {
-    case "1":
-      oro =
-        prompt(`Elija el numero que corresponde para elejir el material de la Joya
-            1. Material: ${JOYAS[0].material.material1}
-            2. Material: ${JOYAS[0].material.material2}
-            `);
-      COMPRASCARRITO.push(JOYAS[0])
-      console.log(COMPRASCARRITO)
-      informeCompra("1")
-      break;
-    case "2":
-      oro =
-        prompt(`Elija el numero que corresponde para elejir el material de la Joya
-              1. Material: ${JOYAS[1].material.material1}
-              2. Material: ${JOYAS[1].material.material2}
-              `);
-      COMPRASCARRITO.push(JOYAS[1])
-      console.log(COMPRASCARRITO)
-      informeCompra("2")
-      break;
-    case "3":
-      oro =
-        prompt(`Elija el numero que corresponde para elejir el material de la Joya
-                1. Material: ${JOYAS[2].material.material1}
-                2. Material: ${JOYAS[2].material.material2}
-                `);
-      COMPRASCARRITO.push(JOYAS[2])
-      console.log(COMPRASCARRITO)
-      informeCompra("3")
-      break;
-    case "4":
-      oro =
-        prompt(`Elija el numero que corresponde para elejir el material de la Joya
-                  1. Material: ${JOYAS[3].material.material1}
-                  2. Material: ${JOYAS[3].material.material2}
-                  `);
-      COMPRASCARRITO.push(JOYAS[3])
-      console.log(COMPRASCARRITO)
-      informeCompra("4")
-      break;
-    case "5":
-      oro =
-        prompt(`Elija el numero que corresponde para elejir el material de la Joya
-                    1. Material: ${JOYAS[4].material.material1}
-                    2. Material: ${JOYAS[4].material.material2}
-                    `);
-      COMPRASCARRITO.push(JOYAS[4])
-      console.log(COMPRASCARRITO)
-      informeCompra("5")
-      break;
-    default:
-      alert("Por favor ingresá un numero valido para poder seleccionar el material de tu joya");
-      break;
-
-
-      function informeCompra(material) {
-        let seguirComprando = ""
-
-        switch (material) {
-          case "1":
-            seguirComprando = prompt("Queres seguir comprando? Si/No").toLowerCase();
-            if (seguirComprando == "si") {
-
-              eleccionJoyas()
-            } else {
-              alert("Gracias por su compra!")
-            }
-            break;
-          case "2":
-            seguirComprando = prompt("Queres seguir comprando? Si/No").toLowerCase();
-            if (seguirComprando == "si") {
-              eleccionJoyas()
-            } else {
-              alert("Gracias por su compra!")
-            }
-            break;
-          case "3":
-            seguirComprando = prompt("Queres seguir comprando? Si/No").toLowerCase();
-            if (seguirComprando == "si") {
-              eleccionJoyas()
-            } else {
-              alert("Gracias por su compra!")
-            }
-            break;
-          case "4":
-            seguirComprando = prompt("Queres seguir comprando? Si/No").toLowerCase();
-            if (seguirComprando == "si") {
-              eleccionJoyas()
-            } else {
-              alert("Gracias por su compra!")
-            }
-            break;
-          case "5":
-            seguirComprando = prompt("Queres seguir comprando? Si/No").toLowerCase();
-            if (seguirComprando == "si") {
-              eleccionJoyas()
-            } else {
-              alert("Gracias por su compra!")
-            }break;
-
-        }
-
-        COMPRASCARRITO.forEach((producto) => {
-          console.log(`${producto.nombre} - ${producto.precio}`)
-
-          console.log(`El total de compra es: ${totalCompra()}`)
-        })
-
-      }
-      function totalCompra(){
-        let total = 0;
-        COMPRASCARRITO.forEach(JOYAS => total += JOYAS.precio);
-        return total; 
-      }
-  }
-}
-*/
